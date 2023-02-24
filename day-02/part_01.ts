c1c7651ace61a7d7400afb5bbcb01bc04ae75b8f
Alexander Euler <alexandereuler@t-online.de>
2023-02-24 14:07:37 +0100

    Solved day-02

---
+++ day-02/part_01.ts
@@ -0,0 +1,62 @@
+import * as fs from "fs/promises";
+
+type ElfChoice = "A" | "B" | "C";
+type MyChoice = "X" | "Y" | "Z";
+type GameResults = "win" | "draw" | "loss";
+
+const winningMap: { [elf in ElfChoice]: MyChoice } = {
+  // Elf is playing rock (A), I would win with paper (Y)
+  A: "Y",
+  // Elf is playing paper (B), I would win with scissors (Z)
+  B: "Z",
+  // Elf is playing scissors (C), I would win with rock (X)
+  C: "X",
+};
+
+const elfChoiceMyChoiceMap: { [elf in ElfChoice]: MyChoice } = {
+  // Rock
+  A: "X",
+  // Paper
+  B: "Y",
+  // Scissors
+  C: "Z",
+};
+
+const shapeBonusMap: { [elf in MyChoice]: number } = {
+  // Rock
+  X: 1,
+  // Paper
+  Y: 2,
+  // Scissors
+  Z: 3,
+};
+
+const roundBonusMap: { [elf in GameResults]: number } = {
+  win: 6,
+  draw: 3,
+  loss: 0,
+};
+
+const data = await fs.readFile("input.txt", "utf8");
+
+const result = data
+  .trimEnd()
+  .split("\n")
+  .map((round) => {
+    const [elf, me] = round.split(" ");
+    return { elf: elf as ElfChoice, me: me as MyChoice };
+  })
+  .reduce((total, round) => {
+    const shape = shapeBonusMap[round.me];
+
+    const win =
+      elfChoiceMyChoiceMap[round.elf] === round.me
+        ? roundBonusMap.draw
+        : winningMap[round.elf] === round.me
+        ? roundBonusMap.win
+        : roundBonusMap.loss;
+
+    return total + shape + win;
+  }, 0);
+
+console.dir(result);
---
+++ day-02/part_02.ts
@@ -0,0 +1,74 @@
+import * as fs from "fs/promises";
+
+type ElfChoice = "A" | "B" | "C";
+const myPossibleChoices = ["X", "Y", "Z"] as const;
+type MyChoice = typeof myPossibleChoices[number];
+type GameResults = "win" | "draw" | "loss";
+
+const winningMap: { [elf in ElfChoice]: MyChoice } = {
+  // Elf is playing rock (A), I would win with paper (Y)
+  A: "Y",
+  // Elf is playing paper (B), I would win with scissors (Z)
+  B: "Z",
+  // Elf is playing scissors (C), I would win with rock (X)
+  C: "X",
+};
+
+const elfChoiceMyChoiceMap: { [elf in ElfChoice]: MyChoice } = {
+  // Rock
+  A: "X",
+  // Paper
+  B: "Y",
+  // Scissors
+  C: "Z",
+};
+
+const shapeBonusMap: { [elf in MyChoice]: number } = {
+  // Rock
+  X: 1,
+  // Paper
+  Y: 2,
+  // Scissors
+  Z: 3,
+};
+
+const roundBonusMap: { [elf in GameResults]: number } = {
+  win: 6,
+  draw: 3,
+  loss: 0,
+};
+
+const data = await fs.readFile("input.txt", "utf8");
+
+const result = data
+  .trimEnd()
+  .split("\n")
+  .map((round) => {
+    const [elf, me] = round.split(" ");
+    return { elf: elf as ElfChoice, me: me as MyChoice };
+  })
+  .reduce((total, round) => {
+    const currentPossibleChoices: { [gameResult in Exclude<GameResults, "loss">]: MyChoice } = {
+      win: winningMap[round.elf],
+      draw: elfChoiceMyChoiceMap[round.elf],
+    };
+
+    const myChoiceBonus =
+      round.me === "Z"
+        ? // I should win
+          roundBonusMap.win + shapeBonusMap[currentPossibleChoices.win]
+        : round.me === "Y"
+        ? // Should be a draw
+          roundBonusMap.draw + shapeBonusMap[currentPossibleChoices.draw]
+        : // Elf should win
+          roundBonusMap.loss +
+          shapeBonusMap[
+            myPossibleChoices.find(
+              (possibleChoice) => !Object.values(currentPossibleChoices).includes(possibleChoice)
+            ) as MyChoice
+          ];
+
+    return total + myChoiceBonus;
+  }, 0);
+
+console.dir(result);
