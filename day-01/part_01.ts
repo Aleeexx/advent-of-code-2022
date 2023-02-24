d9a44912009d0d4af310680fa4278f96f0bd2453
Alexander Euler <alexandereuler@t-online.de>
2023-02-24 14:06:58 +0100

    formats day-01

--- day-01/part_01.ts
+++ day-01/part_01.ts
@@ -1,12 +1,12 @@
-import * as fs from "fs/promises"
+import * as fs from "fs/promises";
 
-const data = await fs.readFile("input.txt", "utf8")
+const data = await fs.readFile("input.txt", "utf8");
 
 const processed = data
   .trimEnd()
   .split("\n\n")
   .map((elf) => elf.split("\n"))
   .map((elf) => elf.reduce((acc, curr) => acc + parseInt(curr), 0))
-  .sort((a, b) => b - a)
+  .sort((a, b) => b - a);
 
-console.dir(processed[0])
+console.dir(processed[0]);
