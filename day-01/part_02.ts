40e2a6bfc1b46eeafbcb2987a67d549e966aaa3a
Alexander Euler <alexandereuler@t-online.de>
2023-02-24 12:27:45 +0100

    Solved day-01

---
+++ day-01/part_02.ts
@@ -0,0 +1,12 @@
+import * as fs from "fs/promises"
+
+const data = await fs.readFile("input.txt", "utf8")
+
+const processed = data
+  .trimEnd()
+  .split("\n\n")
+  .map((elf) => elf.split("\n"))
+  .map((elf) => elf.reduce((acc, curr) => acc + parseInt(curr), 0))
+  .sort((a, b) => b - a)
+
+console.dir(processed[0] + processed[1] + processed[2])
